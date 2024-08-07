5b47f1b41c863cec797bb61aeca69fd8a47b3b3c
Yash Raj <ryash072007@gmail.com>
2024-08-07 22:00:28 +0400

    teacher model created

--- TeacherProject/teacher/models.py
+++ TeacherProject/teacher/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Teacher(models.Model):
+
+    # Main Details
+    email = models.EmailField("Teacher Email Address", max_length=50)
+    password = models.TextField("Teacher Password", max_length=20)
+    phone = models.IntegerField("Teacher Phone no.", max_length=10)
+    name = models.TextField("Teacher Name", max_length=30)
+
+    # Supplementary Details
+    qualifications = models.TextField("Teacher Qualifications", max_length=200)
+
+    # OTP Details
+    onboarded = models.BooleanField("Teacher Onboarded Status", default=False)
+    forgottenPassword = models.BooleanField("Teacher Forgotten Password")
+    otp = models.IntegerField("OTP Password", max_length=4)
+
